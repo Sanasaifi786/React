37178ad3448f3581b6825b796572d835b2af43ce
Sanasaifi786 <sanasaifi1420@gmail.com>
2025-05-14 16:27:19 +0530

    Navbar and props

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar.jsx';
+
+function App() {
+  return (
+    <>
+   <Navbar title="Sana" home="Ghar"/>
+   </>
+  );
+}
+
+export default App;
---
+++ my-app/src/module2.mjs
@@ -0,0 +1,4 @@
+import x, {a,c} from './module1.mjs';
+//import x from './module1.mjs';
+console.log(x);
+console.log(a,x,c);
